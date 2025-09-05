05e61dbe5f59e903565a323605d3fd73f3229445
Joshua Smith <joshuasmith@byui.edu>
2025-09-04 23:28:01 -0600

    changed search to set

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const static = require("./routes/static")
 /* ***********************
  * View Engine and Templates
  *************************/
-app.search("view engine", "ejs")
+app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at view root
 

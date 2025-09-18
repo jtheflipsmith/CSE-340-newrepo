62754166714caf4e8437d7c358264a7581d513e2
Joshua Smith <joshuasmith@byui.edu>
2025-09-17 20:05:16 -0600

    minor fixes

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ app.use("/inv", inventoryRoute)
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT 
-const host = process.env.HOST
+const port = process.env.PORT || 3000
+const host = process.env.HOST || "localhost"
 
 /* ***********************
  * Log statement to confirm server operation
